9b9f5e9021003a91c8509b098ee56ca3bee46d14
Hiroki Chiyoda <contact@arafipro.com>
2023-11-23 20:11:18 +0900

    フォームコンポーネントを追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,94 @@
+"use client";
+
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import * as z from "zod";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  apiKey: z.string().length(51, {
+    message: "OPENAI API KEYを入力してください",
+  }),
+  pdfFile: z
+    .string()
+    .regex(RegExp("^.*\\.pdf$"), "PDFファイルを指定してください"),
+  question: z.string().min(2, {
+    message: "質問を入力してください",
+  }),
+});
+
 export default function Home() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      apiKey: "",
+      pdfFile: "",
+      question: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <h1 className="text-5xl">Hello, World</h1>
-    </main>
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="apiKey"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>OPENAI APIKEY</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="OPENAI APIKEYを入力してください"
+                  type="password"
+                  {...field}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="pdfFile"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>PDFファイル</FormLabel>
+              <FormControl>
+                <Input type="file" accept=".pdf" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="question"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>質問</FormLabel>
+              <FormControl>
+                <Input placeholder="質問を入力してください" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Submit</Button>
+      </form>
+    </Form>
   );
 }
